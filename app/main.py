3180f26f4225c71731404aaccb29b589c7557699
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2022-11-19 02:44:31 +0530

    add cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, Body
 from slowapi.errors import RateLimitExceeded
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "*",
+]
 
 
 limiter = Limiter(key_func=get_remote_address)
@@ -9,12 +15,23 @@ app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/home")
-@limiter.limit("10/minute")
+@limiter.limit("15/minute")
 async def homepage(request: Request):
     return "test"
 
-@app.get("/mars")
-@limiter.limit("10/minute")
-async def homepage(request: Request, response: Response):
+@app.post("/generate")
+@limiter.limit("15/minute")
+async def homepage(request: Request):
+    print(await request.json())
+    import time
+    time.sleep(2)
     return {"key": "value"}
