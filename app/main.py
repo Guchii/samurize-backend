953cdcd2a43711dec11e777a1a6abe9c757e9568
jupeeter8 <aswalanirudh@outlook.in>
2022-11-19 12:43:57 +0530

    feature/Added API to summarize paragraphs

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ from slowapi.errors import RateLimitExceeded
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from .scraper import getPara
-
+from .schema import Payload
+from app import clean
+from .model import query
 from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
@@ -41,3 +43,9 @@ async def homepage(request: Request):
 async def wikiscraper(url : str = "https://en.wikipedia.org/wiki/Electron"):
     dic = getPara(url)
     return dic
+
+@app.post("/gensum")
+async def generate_summary(paylaod):
+    body = paylaod
+    output = query({"inputs" : body})
+    return output
